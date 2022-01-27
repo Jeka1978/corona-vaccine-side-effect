11a0e80ab47fa524ab90987e42c2b1dae94b711a
jeka1978 <kit2009>
2022-01-27 15:49:52 +0200

    test improved

--- src/test/java/com/kitarik/vaccine_side_effect/VaccineSideEffectRegistrarTest.java
+++ src/test/java/com/kitarik/vaccine_side_effect/VaccineSideEffectRegistrarTest.java
@@ -1,31 +1,47 @@
 package com.kitarik.vaccine_side_effect;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 /**
  * @author Evgeny Borisov
  */
 public class VaccineSideEffectRegistrarTest {
 
+    private final PrintStream standardOut = System.out;
+    private ByteArrayOutputStream consoleContainer;
+    private VaccineSideEffectRegistrar registrar;
 
-    @Test
-    public void testCountLogic() {
-        VaccineSideEffectRegistrar registrar = new VaccineSideEffectRegistrar();
+    @Before
+    public void setUp() {
+        consoleContainer = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(consoleContainer));
+        registrar = new VaccineSideEffectRegistrar();
         registrar.registerSideEffect("chill");
         registrar.registerSideEffect("temperature");
         registrar.registerSideEffect("cough");
-        registrar.printAll();
+    }
 
-
+    @Test
+    public void testCount() {
         Assert.assertEquals(3, registrar.count());
-
         registrar.registerSideEffect("jopabolit");
-        Assert.assertEquals(4,registrar.count());
+        Assert.assertEquals(4, registrar.count());
+    }
 
-        System.out.println("URA!!!!");
+    @Test
+    public void testCountLogic() {
+
+        registrar.printAll();
+        String output = consoleContainer.toString();
+        Assert.assertTrue(output.contains("chill"));
+        Assert.assertTrue(output.contains("temperature"));
+        Assert.assertTrue(output.contains("cough"));
+
 
     }
 
